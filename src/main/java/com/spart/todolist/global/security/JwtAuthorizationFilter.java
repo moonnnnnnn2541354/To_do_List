d9a04befef7e425d519d34a626115aca7bd37847
tast <ioae7979@gmail.com>
2023-11-30 20:31:09 +0900

    feat: doFilterInternal (인증정보에 유저정보 넣기) 기능 구현

---
+++ src/main/java/com/spart/todolist/global/security/JwtAuthorizationFilter.java
@@ -0,0 +1,54 @@
+package com.spart.todolist.global.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spart.todolist.domain.user.service.UserService;
+import com.spart.todolist.global.dto.CommonResponseDto;
+import com.spart.todolist.global.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final ObjectMapper objectMapper;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtUtil.createToken(request);
+
+        if (Objects.nonNull(token)) {
+            if (jwtUtil.validateToken(token)) {
+                Claims info = jwtUtil.getUserInfoFromToken(token);
+
+                String username = info.getSubject();
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                UserDetails userDetails = userService.getUserDetails(username);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails,null);
+                context.setAuthentication(authentication);
+            } else {
+                CommonResponseDto commonResponseDto = new CommonResponseDto("토큰이 유효하지 않습니다.",
+                    HttpStatus.BAD_REQUEST.value());
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.setContentType("application/json; charset=UTF-8");
+                response.getWriter().write(objectMapper.writeValueAsString(commonResponseDto));
+            }
+        }
+
+    }
+}
