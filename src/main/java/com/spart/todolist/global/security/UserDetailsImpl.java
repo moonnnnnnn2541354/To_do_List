c8ea24ae7b887a6def78bd9278617fb8185e14fd
tast <ioae7979@gmail.com>
2023-11-30 20:14:27 +0900

    feat: UserDetailsImpl.java 구현

---
+++ src/main/java/com/spart/todolist/global/security/UserDetailsImpl.java
@@ -0,0 +1,48 @@
+package com.spart.todolist.global.security;
+
+import com.spart.todolist.domain.user.entity.User;
+import java.util.Collection;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
