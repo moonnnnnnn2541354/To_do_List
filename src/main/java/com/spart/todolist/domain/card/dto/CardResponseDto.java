6baef58d23d548c4e8ac0559edec32bee6505134
tast <ioae7979@gmail.com>
2023-12-01 16:26:29 +0900

    refactor: getContent 코드 수정

--- src/main/java/com/spart/todolist/domain/card/dto/CardResponseDto.java
+++ src/main/java/com/spart/todolist/domain/card/dto/CardResponseDto.java
@@ -1,7 +1,6 @@
 package com.spart.todolist.domain.card.dto;
 
 import com.spart.todolist.domain.card.entity.Card;
-import com.spart.todolist.domain.user.entity.User;
 import lombok.Getter;
 
 @Getter
@@ -13,7 +12,7 @@ public class CardResponseDto {
 
     public CardResponseDto(Card card) {
         this.title = card.getTitle();
-        this.content = getContent();
+        this.content = card.getContent();
         this.username = card.getUser().getUsername();
     }
 }
