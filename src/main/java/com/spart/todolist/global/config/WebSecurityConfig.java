053ef62ab3b3848293b9bd99d9dee87ba8b04f79
tast <ioae7979@gmail.com>
2023-12-01 10:22:07 +0900

    feat: securityFilterChain 기능 구현

--- src/main/java/com/spart/todolist/global/config/WebSecurityConfig.java
+++ src/main/java/com/spart/todolist/global/config/WebSecurityConfig.java
@@ -1,15 +1,53 @@
 package com.spart.todolist.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spart.todolist.domain.user.service.UserService;
+import com.spart.todolist.global.jwt.JwtUtil;
+import com.spart.todolist.global.security.JwtAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final ObjectMapper objectMapper;
+    private final UserService userService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new JwtAuthorizationFilter(jwtUtil,objectMapper,userService);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf((csrf) -> csrf.disable());
+
+        httpSecurity.sessionManagement(
+            (sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        httpSecurity.authorizeHttpRequests(
+            (authorizeHttpRequests) -> authorizeHttpRequests
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .requestMatchers("/api/user/**").permitAll()
+                .anyRequest().authenticated());
+
+        httpSecurity.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
     }
 }
