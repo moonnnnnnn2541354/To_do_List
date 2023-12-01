d18e6446dd89099fa7048eae3ad0e2c93472fcf4
tast <ioae7979@gmail.com>
2023-12-01 15:48:53 +0900

    feat: 로그인 메소드 구현

--- src/main/java/com/spart/todolist/domain/user/controller/UserController.java
+++ src/main/java/com/spart/todolist/domain/user/controller/UserController.java
@@ -3,10 +3,14 @@ package com.spart.todolist.domain.user.controller;
 import com.spart.todolist.domain.user.dto.UserRequestDto;
 import com.spart.todolist.domain.user.service.UserService;
 import com.spart.todolist.global.dto.CommonResponseDto;
+import com.spart.todolist.global.jwt.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/signup")
     public ResponseEntity<CommonResponseDto> signup(@Valid @RequestBody UserRequestDto requestDto) {
@@ -30,5 +36,19 @@ public class UserController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(new CommonResponseDto("회원가입 성공", HttpStatus.CREATED.value()));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<CommonResponseDto> login(@Valid @RequestBody UserRequestDto requestDto,
+        HttpServletResponse response) {
+        try {
+            userService.login(requestDto);
+        } catch (IllegalArgumentException exception) {
+            return ResponseEntity.badRequest()
+                .body(new CommonResponseDto("로그인 실패", HttpStatus.BAD_REQUEST.value()));
+        }
+        response.setHeader(JwtUtil.AUTHORIZATION_HEADER,
+            jwtUtil.createToken(requestDto.getUsername()));
+        return ResponseEntity.ok().body(new CommonResponseDto("로그인 성공", HttpStatus.OK.value()));
     }
 }
