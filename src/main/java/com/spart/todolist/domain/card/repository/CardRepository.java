b4a405d0602048e1e0ad59bdc635326460b62c41
tast <ioae7979@gmail.com>
2023-12-01 16:16:22 +0900

    feat: CardRepository 구현

---
+++ src/main/java/com/spart/todolist/domain/card/repository/CardRepository.java
@@ -0,0 +1,10 @@
+package com.spart.todolist.domain.card.repository;
+
+import com.spart.todolist.domain.card.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card,Long> {
+
+}
