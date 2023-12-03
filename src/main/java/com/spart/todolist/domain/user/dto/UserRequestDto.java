e24ca3ed7d53e0e0b1546e9d287a77f0b6e4fb73
tast <ioae7979@gmail.com>
2023-12-03 23:20:14 +0900

    refactor: 생성자 주입 어노테이션 추가

--- src/main/java/com/spart/todolist/domain/user/dto/UserRequestDto.java
+++ src/main/java/com/spart/todolist/domain/user/dto/UserRequestDto.java
@@ -1,9 +1,13 @@
 package com.spart.todolist.domain.user.dto;
 
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRequestDto {
 
     @Pattern(regexp = "^[a-z0-9]{4,10}$")
