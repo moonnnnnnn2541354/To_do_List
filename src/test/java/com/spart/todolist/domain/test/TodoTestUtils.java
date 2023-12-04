e059d71e8618ca8f95db0645cd30edce686399e8
tast <ioae7979@gmail.com>
2023-12-04 20:14:40 +0900

    refactor: createDate부분 삭제

--- src/test/java/com/spart/todolist/domain/test/TodoTestUtils.java
+++ src/test/java/com/spart/todolist/domain/test/TodoTestUtils.java
@@ -11,21 +11,19 @@ import org.springframework.util.SerializationUtils;
 public class TodoTestUtils {
 
     public static Card get(Card card, User user) {
-        return get(card, 1L, LocalDateTime.now(), user);
+        return get(card, 1L, user);
     }
 
     /**
      * 테스트용 할일 객체를 만들어주는 메서드
      * @param card 복제할 할일 객체
      * @param id 설정할 아이디
-     * @param createDate 설정할 생성일시
      * @param user 연관관계 유저
      * @return 테스트용으로 생성된 할일 객체
      */
-    public static Card get(Card card, Long id, LocalDateTime createDate, User user) {
+    public static Card get(Card card, Long id, User user) {
         var newTodo = SerializationUtils.clone(card);
         ReflectionTestUtils.setField(newTodo, Card.class, "id", id, Long.class);
-        ReflectionTestUtils.setField(newTodo, Card.class, "createDate", createDate, LocalDateTime.class);
         newTodo.setUser(user);
         return newTodo;
     }
