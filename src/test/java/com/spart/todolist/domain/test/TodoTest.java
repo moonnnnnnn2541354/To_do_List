12aa0179615217422048975235f50ba04ad420d9
tast <ioae7979@gmail.com>
2023-12-04 19:23:05 +0900

    feat: TodoTest.java 구현

---
+++ src/test/java/com/spart/todolist/domain/test/TodoTest.java
@@ -0,0 +1,59 @@
+package com.spart.todolist.domain.test;
+
+
+import com.spart.todolist.domain.card.dto.CardRequestDto;
+import com.spart.todolist.domain.card.dto.CreateCardResponseDto;
+import com.spart.todolist.domain.card.dto.SelectCardResponseDto;
+import com.spart.todolist.domain.card.dto.UpdateCardResponseDto;
+import com.spart.todolist.domain.card.entity.Card;
+import java.time.LocalDateTime;
+
+public interface TodoTest extends CommonTest {
+
+    Long TEST_TODO_ID = 1L;
+    String TEST_TODO_TITLE = "title";
+    String TEST_TODO_CONTENT = "content";
+    Boolean DEFAULT_COMPLETE = false;
+    LocalDateTime DATE_TIME = LocalDateTime.now();
+
+    CardRequestDto TEST_TODO_REQUEST_DTO = CardRequestDto.builder()
+        .title(TEST_TODO_TITLE)
+        .content(TEST_TODO_CONTENT)
+        .build();
+
+    CreateCardResponseDto TEST_TODO_CREATE_RESPONSE_DTO = CreateCardResponseDto.builder()
+        .title(TEST_TODO_TITLE)
+        .content(TEST_TODO_CONTENT)
+        .username(CommonTest.TEST_USER_NAME)
+        .date(DATE_TIME)
+        .complete(DEFAULT_COMPLETE)
+        .build();
+
+    UpdateCardResponseDto TEST_TODO_UPDATE_RESPONSE_DTO = UpdateCardResponseDto.builder()
+        .title(TEST_TODO_TITLE)
+        .content(TEST_TODO_CONTENT)
+        .username(CommonTest.TEST_USER_NAME)
+        .date(DATE_TIME)
+        .complete(DEFAULT_COMPLETE)
+        .build();
+
+    SelectCardResponseDto TEST_TODO_SELECT_RESPONSE_DTO = SelectCardResponseDto.builder()
+        .title(TEST_TODO_TITLE)
+        .content(TEST_TODO_CONTENT)
+        .username(CommonTest.TEST_USER_NAME)
+        .date(DATE_TIME)
+        .complete(DEFAULT_COMPLETE)
+        .build();
+    Card TEST_TODO = Card.builder()
+        .title(TEST_TODO_TITLE)
+        .content(TEST_TODO_CONTENT)
+        .user(CommonTest.TEST_USER)
+        .complete(DEFAULT_COMPLETE)
+        .build();
+    Card TEST_ANOTHER_TODO = Card.builder()
+        .title(ANOTHER_PREFIX + TEST_TODO_TITLE)
+        .content(ANOTHER_PREFIX + TEST_TODO_CONTENT)
+        .user(CommonTest.TEST_ANOTHER_USER)
+        .complete(DEFAULT_COMPLETE)
+        .build();
+}
