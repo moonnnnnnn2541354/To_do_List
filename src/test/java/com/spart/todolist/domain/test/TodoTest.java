31cd884eadc21b6db4e467ac9c4e9baca26f6a78
tast <ioae7979@gmail.com>
2023-12-04 21:03:12 +0900

    feat: TodoTest.java Comment추가

--- src/test/java/com/spart/todolist/domain/test/TodoTest.java
+++ src/test/java/com/spart/todolist/domain/test/TodoTest.java
@@ -6,13 +6,18 @@ import com.spart.todolist.domain.card.dto.CreateCardResponseDto;
 import com.spart.todolist.domain.card.dto.SelectCardResponseDto;
 import com.spart.todolist.domain.card.dto.UpdateCardResponseDto;
 import com.spart.todolist.domain.card.entity.Card;
+import com.spart.todolist.domain.comment.dto.CommentResponseDto;
+import com.spart.todolist.domain.comment.entity.Comment;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 public interface TodoTest extends CommonTest {
 
     Long TEST_TODO_ID = 1L;
     String TEST_TODO_TITLE = "title";
     String TEST_TODO_CONTENT = "content";
+    String TEST_COMMENT_CONTENT = "comment";
     Boolean DEFAULT_COMPLETE = false;
     LocalDateTime DATE_TIME = LocalDateTime.now();
 
@@ -55,5 +60,11 @@ public interface TodoTest extends CommonTest {
         .content(ANOTHER_PREFIX + TEST_TODO_CONTENT)
         .user(CommonTest.TEST_ANOTHER_USER)
         .complete(DEFAULT_COMPLETE)
+        .build();
+
+    Comment COMMENT = Comment.builder()
+        .card(TEST_TODO)
+        .user(TEST_USER)
+        .content(TEST_COMMENT_CONTENT)
         .build();
 }
